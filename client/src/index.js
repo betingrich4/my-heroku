ecacc3820aa64a3d403b8b81a9531d7e226c8195
betingrich4 <mariseltru@gmail.com>
2025-05-25 12:41:32 +0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,4 @@
+import reportWebVitals from './reportWebVitals';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -12,6 +13,6 @@ root.render(
     </ThemeProvider>
   </React.StrictMode>
 );
-
+reportWebVitals(console.log);  // Or pass your analytics function
 // Remove or comment out the reportWebVitals import and call
 // (This is the line causing your error)
