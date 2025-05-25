bd6326b74c1398be04cd7d378ff47b0143d4abfe
betingrich4 <mariseltru@gmail.com>
2025-05-25 12:35:36 +0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { ThemeProvider } from './context/ThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,6 +13,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-reportWebVitals();
+// Remove or comment out the reportWebVitals import and call
+// (This is the line causing your error)
